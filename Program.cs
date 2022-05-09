1c2b9f11fa874a09f8fbcec509752379d20ce880
kirill <kirill.bychkov@list.ru>
2022-05-09 14:57:50 +0700

    dollarToRub

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace MyFirstGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double rub, sum;
+            const double kurs_dollar = 70;
+
+            Console.WriteLine("Введите кол-во $: ");
+
+            rub = double.Parse(Console.ReadLine());
+
+            sum = rub * kurs_dollar;
+
+            Console.WriteLine("$" + rub + " = " + sum);
+            
+            Console.ReadLine();
+  
         }
     }
 }
